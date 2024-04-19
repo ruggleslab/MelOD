99634520db435a1d5116bc3c01e0bad829ea8f48
Polochoonn <p.sastourne@outlook.com>
2024-04-19 16:16:39 -0400

    change structure files and add test plot for bulkrna visualisation

--- ui.R
+++ ui.R
@@ -6,54 +6,19 @@ library(bsicons)
 ui <- fluidPage(
   theme = shinythemes::shinytheme("sandstone"),
   includeCSS("./www/styles.css"),
-  navbarPage(
-    "ShinySeq",
+  navbarPage("ShinySeq",
     collapsible=TRUE,
-    tabPanel(
-      "Query",
-      sidebarLayout(
-        sidebarPanel(
-          textInput("gene_search", "Search Gene:"),
-          p("OR"),
-          fileInput("file", "Upload CSV file", multiple = TRUE),
-          
-          # Help text
-          tags$br(),
-          helpText("Default max file size is 5MB"),
-          helpText("Select read.table parameters below:"),
-          
-          # Checkbox for header
-          checkboxInput(
-            inputId = "header",
-            label = "Header",
-            value = TRUE
-          ),
-          
-          
-          # Radio buttons for separator
-          radioButtons(
-            inputId = "sep",
-            label = "Separator",
-            choices = c(
-              Comma = ",",
-              Semicolon = ";",
-              Tab = "\t",
-              Space = " "
-            ),
-            selected = ","
-          ),
-          uiOutput("selectfile")
-          
-        ),
-        
-        
-        mainPanel(# Table output
-          tableOutput("tb"))
-      )
-    ),
+    
+############## TAB PANEL Import ###############
+
+    tabPanel("Import",
+             import_ui("import_tab")),
+
+############## TAB PANEL BULK RNA ###############
+
     tabPanel("Bulk RNA",
-             plotOutput("bulk_rna_boxplot"),
-             plotOutput("bulk_rna_correlation")),
+             bulkrna_ui("bulkrna_tab")),
+             
     tabPanel("scRNA-SEQ",
              plotOutput("scrna_boxplot"),
              plotOutput("scrna_correlation")),
